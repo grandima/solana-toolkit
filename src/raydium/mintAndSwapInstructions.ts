1ec66de327a17f0b0162557c466ff54f326fc482
Dmytro Medynskyi <grandima@gmail.com>
2024-04-01 15:36:29 +0200

    add mintAndSwapInstructions

--- src/raydium/mintAndSwapInstructions.ts
+++ src/raydium/mintAndSwapInstructions.ts
@@ -1,16 +1,38 @@
 import { createMintToCheckedInstruction } from '@solana/spl-token'
-import { Token } from '@raydium-io/raydium-sdk'
+import { LiquidityPoolKeys, Token, TokenAccount, TokenAmount, TxVersion } from '@raydium-io/raydium-sdk'
 import { Connection, PublicKey } from '@solana/web3.js'
+import { makeSwapInstructionSimple } from './swapAMMSync'
+
+export const makeTxVersion = TxVersion.V0;
 
 export const mintAndSwapInstructions = async (
   connection: Connection,
+  poolKeys: LiquidityPoolKeys,
+  baseToken: Token,
+  leftAmount: number,
+  quoteToken: Token,
+  masterWalletTokenAccounts: TokenAccount[],
   tokenAccount: PublicKey,
   authority: PublicKey,
-  sellInfo: {token: Token, amount: number}) => {
-
-  //TODO: add makeSwapInstructionSimple
-
-
+  sellInfo: {
+    token: Token,
+    amount: number
+  }
+  ) => {
+  const sellInstructions = (await makeSwapInstructionSimple({
+    connection,
+    poolKeys,
+    userKeys: {
+      tokenAccounts: masterWalletTokenAccounts,
+      owner: authority,
+    },
+    amountIn: new TokenAmount(baseToken, leftAmount),
+    //TODO: figure out what should be this amountOut
+    amountOut: new TokenAmount(quoteToken, 0.002 * (10 ** quoteToken.decimals)),
+    fixedSide: 'in',
+    makeTxVersion,
+  }))
+    .innerTransactions[0]
   const mintInstruction = createMintToCheckedInstruction(
     sellInfo.token.mint,
     tokenAccount,
@@ -18,4 +40,6 @@ export const mintAndSwapInstructions = async (
     sellInfo.amount,
     sellInfo.token.decimals
   )
+  sellInstructions.instructions.unshift(mintInstruction)
+  return sellInstructions
 }
